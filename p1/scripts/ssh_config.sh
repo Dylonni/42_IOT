ce436aa1531ed9be8aaec578f191d55bc13443e6
Dylan AUMIS <daumis@f6r11s11.clusters.42paris.fr>
2025-08-20 11:18:55 +0200

    fix: passwordless connection to K3s VMS

--- p1/scripts/ssh_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p /home/vagrant/.ssh
-chown vagrant:vagrant /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
--- p1/scripts/ssh_perm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f /home/vagrant/.ssh/authorized_keys ]; then
-  chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
-  chmod 600 /home/vagrant/.ssh/authorized_keys
-else
-  echo "⚠️  authorized_keys not found."
-  exit 1
-fi
